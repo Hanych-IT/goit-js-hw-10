7183b107e48f3f63151ffbf3256314ba00f58ad7
Dmytro Hanych <Hanych_IT@ukr.net>
2022-10-09 22:18:32 +0300

    added settings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
+
+import { fetchCountries } from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
