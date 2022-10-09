aa2a8653fa70bf5dec91ea2fc8ac0ffc2ee3776e
Dmytro Hanych <Hanych_IT@ukr.net>
2022-10-09 23:25:44 +0300

    update src

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import { Notify } from 'notiflix';
 import debounce from 'lodash.debounce';
 
 import { fetchCountries } from './js/fetchCountries.js';
+import { refs } from './js/refs.js';
+// import countryListTpl from '../src/templates/country-list.hbs';
+// import countryCardTpl from '../src/templates/country-cards.hbs';
 
 const DEBOUNCE_DELAY = 300;
